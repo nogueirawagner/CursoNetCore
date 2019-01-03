c950a3db1fcaaf978fb5f7f5092c268677a12986
Wagner Nogueira <wagner@ephrom.com.br>
2019-01-03 11:12:37 -0200

    Add project files.

---
+++ Curso.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Curso.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
