68d333b6406075c6d50b9c2dfadd1ee4974c8190
Wagner Nogueira <engnogueirawgn@hotmail.com>
2019-01-05 18:04:01 -0200

    criando view components

---
+++ Curso.WebApplication/Models/Carrinho.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso.WebApplication.Models
+{
+    public class Carrinho
+    {
+        public int TotalItens { get; set; }
+        public int ValorTotal { get; set; }
+    }
+}
---
+++ Curso.WebApplication/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Curso.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso.WebApplication.ViewComponents
+{
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // Logica
+            var carrinho = new Carrinho()
+            {
+                TotalItens = 10,
+                ValorTotal = 1400
+            };
+            return View(carrinho);
+        }
+    }
+}
